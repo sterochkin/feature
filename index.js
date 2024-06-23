da22b83d1b13d1d8516b64c1b5fba8ddcefa0456
Maksim <brezgin1@gmail.com>
2024-06-23 14:09:47 +0300

    Первый коммит

---
+++ index.js
@@ -0,0 +1,8 @@
+let promt = prompt('Введите число от 1 до 4 ');
+
+if (promt >=4 && promt !=0){
+    console.log('Вы ввели число' +promt);
+} else {
+    console.log('Вы ввели число не равно 1-4');
+}
+22
